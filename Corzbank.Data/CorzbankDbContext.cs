b2b3cc7798404d1e430f74d4785cdf5e7321d351
Nazar865 <nazaben03@gmail.com>
2022-05-03 17:32:34 +0300

    refactored dbContext

--- Corzbank.Data/CorzbankDbContext.cs
+++ Corzbank.Data/CorzbankDbContext.cs
@@ -20,7 +20,36 @@ namespace Corzbank.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<DepositCard>().HasKey(x => new { x.CardId, x.DepositId });
-            modelBuilder.Entity<TransferCard>().HasKey(x => new { x.TransferId, x.SenderCardId, x.ReceiverCardId});
+            modelBuilder.Entity<TransferCard>().HasKey(x => new { x.TransferId, x.SenderCardId, x.ReceiverCardId });
+
+            modelBuilder.Entity<DepositCard>()
+                .HasOne(x => x.Deposit)
+                .WithMany(y => y.DepositCards)
+                .HasForeignKey(z => z.DepositId);
+
+            modelBuilder.Entity<DepositCard>()
+               .HasOne(x => x.Card)
+               .WithMany(y => y.DepositCards)
+               .HasForeignKey(z => z.CardId);
+
+
+
+            modelBuilder.Entity<TransferCard>()
+                   .HasOne(x => x.Transfer)
+                   .WithMany(y => y.TransferCards)
+                   .HasForeignKey(z => z.TransferId);
+
+            modelBuilder.Entity<TransferCard>()
+               .HasOne(x => x.Card)
+               .WithMany(y => y.TransferCards)
+               .HasForeignKey(z => z.ReceiverCardId)
+               .IsRequired(false);
+
+            modelBuilder.Entity<TransferCard>()
+               .HasOne(x => x.Card)
+               .WithMany(y => y.TransferCards)
+               .HasForeignKey(z => z.SenderCardId)
+               .IsRequired(false);
         }
 
         public DbSet<User> Users { get; set; }
